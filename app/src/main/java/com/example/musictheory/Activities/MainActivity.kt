def9ba3390e493c9c4542dfe1904757d1532e7bc
hvendrix <hvendrix@gmail.com>
2021-02-06 23:33:30 +0300

    plus minus stable version

--- app/src/main/java/com/example/musictheory/Activities/MainActivity.kt
+++ app/src/main/java/com/example/musictheory/Activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.musictheory.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.musictheory.R
 import com.example.musictheory.models.MyViewModel
@@ -12,7 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
 //        var l = mutableListOf<String>()
 //        l.add("asd")
 
@@ -38,6 +42,12 @@ class MainActivity : AppCompatActivity() {
 //        binding.myViewModel = myViewModel
 
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+//        return super.onSupportNavigateUp()
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 
 
--- app/src/main/java/com/example/musictheory/Fragments/TheoryFragment.kt
+++ app/src/main/java/com/example/musictheory/Fragments/TheoryFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.example.musictheory.R
+import com.example.musictheory.databinding.FragmentResultBinding
+import com.example.musictheory.databinding.FragmentTheoryBinding
 import kotlinx.android.synthetic.main.fragment_theory.*
 
 
@@ -15,12 +19,16 @@ class TheoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        var textHelp ="text"
-//        txtTheory.setText(textHelp)
+        val binding : FragmentTheoryBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_theory, container, false)
+//         Замена
+//         Inflate the layout for this fragment
+//                var rootView = inflater.inflate(R.layout.fragment_theory, container, false)
+//                return rootView
+        binding.lifecycleOwner = this
+        var textHelp = "text"
+        binding.helpText = textHelp
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_theory, container, false)
-
+        return binding.root
 
     }
 
